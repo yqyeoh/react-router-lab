9f681427a92c150e984b806e33e6a17d963f3535
yqyeoh <yqyeoh@hotmail.com>
2019-02-28 11:03:35 +0800

    exploring history object

--- src/components/TacosFeed/TacosFeed.jsx
+++ src/components/TacosFeed/TacosFeed.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function TacosFeed({match}) {
+function TacosFeed({match, history}) {
+  console.log(history)
+
+  const onClickBack = () =>{
+      history.push('twitter')
+  }
+
+  const onClickForward = () =>{
+    history.go(2)
+}
+
   return (
     <div>
       <h1>This is Tacos Feed</h1>
       <h2>{match.params.id}</h2>
       <h2>{match.params.userid}</h2>
+      <button onClick={onClickBack}>Back</button>
+      <button onClick={onClickForward}>Forward</button>
     </div>
   )
 }
